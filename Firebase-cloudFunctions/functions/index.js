44b59954aff31c9eb61e63e1487589fa3c83adf1
Rishabh Sharma <rishabhsharma3617@gmail.com>
2020-03-30 14:18:42 +0530

    Added the callable and endpoint functions

--- Firebase-cloudFunctions/functions/index.js
+++ Firebase-cloudFunctions/functions/index.js
@@ -6,3 +6,20 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+//Http request 1  ---@ endpoint type of function
+exports.randomNumber = functions.https.onRequest((req,res) => {
+    const number = Math.round(Math.random() * 100)
+    console.log(number)
+    res.send(number.toString())
+})
+
+//http request 2 --@endpoint 
+exports.toGithub = functions.https.onRequest((req,res) => {
+    res.redirect('https://github.com/rishabhsharma3617')
+})
+
+//http Callable functions
+exports.sayHello = functions.https.onCall((data,context) => {
+    return `hello ${data.name}`
+})
--- Firebase-cloudFunctions/public/js/app.js
+++ Firebase-cloudFunctions/public/js/app.js
@@ -6,6 +6,15 @@ requestLink.addEventListener('click' , () => {
     requestModal.classList.add('open')
 })
 
+//say hello function
+const button = document.querySelector('.call')
+button.addEventListener('click' , () => {
+    const sayHello = firebase.functions().httpsCallable('sayHello')
+    sayHello({name : 'Rishabh'}).then(result => {
+        console.log(result.data)
+    })
+})
+
 requestModal.addEventListener('click' , (e) => {
     if(e.target.classList.contains('new-request')){
         //this is condition is embeded as if the user clicks otside the modal which we have
